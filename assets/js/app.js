7333a7a6f9c690ad6a47dded8122723645599ccf
soumyapatra123 <soumyapatra@eagletech.in>
2023-06-04 17:23:22 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -173,7 +173,7 @@ let footer = $(`
         </svg>
       </a>
 
-      <a class="social-button github" href="https://github.com/soumyapatra123" target="_blank">
+      <a class="social-button github" href="https://github.com/patrasoumyaranjan1999" target="_blank">
       <!-- SVG code for Github icon -->
       <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
           <!-- body -->
